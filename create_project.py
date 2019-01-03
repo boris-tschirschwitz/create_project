22b1302a26307444e057b9894462945adc4b96be
Boris <borist@mac.com>
2019-01-03 10:33:35 +0100

    Also copy gitignore into new project.

--- create_project.py
+++ create_project.py
@@ -1,25 +1,32 @@
-import os
-import shutil
+from os import path, mkdir
+from shutil import copy
 
 
 def create_project(name):
     """
-    Create a folder 'name' and a basic Makefile inside.
+    Create a project folder skeleton named 'name'.
+
+    Copy a basic Makefile and gitignore into the folder.
 
     :param name: The project name.
     """
 
     create_folder(name)
     copy_makefile(name)
+    copy_gitignore(name)
 
 
 def create_folder(name):
-    os.mkdir(name)
+    mkdir(name)
 
 
 def copy_makefile(name):
-    makefile_path = os.path.join(os.path.dirname(__file__), 'Makefile')
-    shutil.copy(makefile_path, name)
+    makefile_path = path.join(path.dirname(__file__), 'Makefile')
+    copy(makefile_path, name)
+
+def copy_gitignore(name):
+    gitignore_path = path.join(path.dirname(__file__), '.gitignore')
+    copy(gitignore_path, name)
 
 
 if __name__ == "__main__":
